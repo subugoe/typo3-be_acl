f4aa0e3cfe8400dda6e3a789c031e22acb84b64c
Ali Reza Sajedi <sajedi@sub.uni-goettingen.de>
2014-02-20 16:24:12 +0100

    First fix for module (ADWD-905)

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
-t3lib_extMgm::allowTableOnStandardPages("tx_beacl_acl");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages("tx_beacl_acl");
 
 $TCA["tx_beacl_acl"] = Array (
 	"ctrl" => Array (
@@ -12,8 +12,8 @@ $TCA["tx_beacl_acl"] = Array (
 		"cruser_id" => "cruser_id",
 		"type" => "type",
 		"default_sortby" => "ORDER BY type",
-		"dynamicConfigFile" => t3lib_extMgm::extPath($_EXTKEY)."tca.php",
-		"iconfile" => t3lib_extMgm::extRelPath($_EXTKEY)."icon_tx_beacl_acl.gif",
+		"dynamicConfigFile" => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)."tca.php",
+		"iconfile" => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY)."icon_tx_beacl_acl.gif",
 	),
 	"feInterface" => Array (
 		"fe_admin_fieldList" => "type, object_id, permissions, recursive",
