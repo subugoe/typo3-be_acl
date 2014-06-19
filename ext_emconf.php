a4f6072b7c25255776451c0127e7910a178412ab
Nicole Cordes <cordes@cps-it.de>
2014-06-19 09:46:40 +0200

    [TASK] Synchronization with local files

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "be_acl".
+#
+# Auto generated 25-03-2012 18:20
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Backend ACLs',
+	'description' => 'Backend Access Control Lists',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '1.4.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '3.5.0-0.0.0',
+	'PHP_version' => '3.0.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Sebastian Kurfuerst',
+	'author_email' => 'sebastian@garbage-group.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
