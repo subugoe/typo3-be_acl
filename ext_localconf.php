309f542af7ef927cc67f8aeea05ef35c45165b5c
Nicole Cordes <cordes@cps-it.de>
2014-06-19 15:22:47 +0200

    [FEATURE] Support for TYPO3 6.2

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,8 +12,14 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['calcPerms
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauthgroup.php']['getPagePermsClause'][] = 'tx_beacl_userAuthGroup->getPagePermsClause';
 
 
+if (version_compare(TYPO3_branch, '6.0', '<')) {
+	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/mod/web/perm/index.php'] = t3lib_extMgm::extPath('be_acl') . 'res/class.ux_sc_mod_web_perm_index.php';
+} else {
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Perm\\Controller\\PermissionModuleController'] = array(
+		'className' => 'Tx_BeAcl_Xclass_PermissionModuleController',
+	);
+}
 
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['typo3/mod/web/perm/index.php'] = t3lib_extMgm::extPath('be_acl').'res/class.ux_sc_mod_web_perm_index.php';
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:be_acl/class.tx_beacl_hooks.php:tx_beacl_hooks';
 
